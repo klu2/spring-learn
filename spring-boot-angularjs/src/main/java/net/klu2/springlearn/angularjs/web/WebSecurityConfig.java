b1907ccffbd4e5d010bbb3ff11b1cf3c37e44a85
klehner <klaus.lehner@catalysts.cc>
2013-12-30 23:00:08 +0100

    upgrade to bootstrap 3

--- spring-boot-angularjs/src/main/java/net/klu2/springlearn/angularjs/web/WebSecurityConfig.java
+++ spring-boot-angularjs/src/main/java/net/klu2/springlearn/angularjs/web/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                //.antMatchers("/", "/home").permitAll()
+                .antMatchers("/css/**", "/js/**").permitAll()
                 .anyRequest().authenticated();
         http
                 .formLogin()
